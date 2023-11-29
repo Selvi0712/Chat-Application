40c032c3a18dc506cad923850cbe71ea9a98365a
Selvi0712 <135791060+Selvi0712@users.noreply.github.com>
2023-11-29 17:30:08 +0530

    Add files via upload

---
+++ chatApp/chatApp/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace chatApp
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;
+        EndPoint eplocal, epRemote;
+        byte[] buffer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listMessage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            textLocalIp.Text = GetLocalIp();
+            textRemoteIp.Text = GetLocalIp();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            eplocal = new IPEndPoint(IPAddress.Parse(textLocalIp.Text), Convert.ToInt32(textLocalPort.Text));
+            sck.Bind(eplocal);
+
+            epRemote = new IPEndPoint(IPAddress.Parse(textRemoteIp.Text), Convert.ToInt32(textRemotePort.Text));
+            sck.Connect(epRemote);
+
+            buffer = new byte[1500];
+            sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback
+                (MessagecallBack), buffer);
+
+        }
+        private void MessagecallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                byte[] receiveddata = new byte[1500];
+                receiveddata = (byte[])aResult.AsyncState;
+
+                ASCIIEncoding aEncoding = new ASCIIEncoding();
+                string receivedmsg = aEncoding.GetString(receiveddata);
+
+                listMessage.Items.Add("Friend: " + receivedmsg);
+                buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback
+                    (MessagecallBack), buffer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            byte[] sendingMessage = new byte[1500];
+            sendingMessage = aEncoding.GetBytes(textMessage.Text);
+
+            sck.Send(sendingMessage);
+
+            listMessage.Items.Add("Me:" + textMessage.Text);
+            textMessage.Text = "";
+        }
+
+        private string GetLocalIp()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+
+            }
+            return "127.0.0.1";
+
+        }
+            
+
+
+
+
+
+        }
+    }
+
